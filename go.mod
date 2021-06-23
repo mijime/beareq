cca0399862e0e2d6cbddb83805b9045188811f24
mijime <07cc0110+gthb@gmail.com>
2021-06-24 08:40:11 +0900

    feat: Add openapi support

--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module github.com/mijime/beareq
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/agnivade/levenshtein v1.1.1
+	github.com/getkin/kin-openapi v0.64.0
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.2.0
+	github.com/iancoleman/strcase v0.1.3
 	github.com/itchyny/gojo v0.1.3
-	github.com/itchyny/gojq v0.12.3
-	github.com/pelletier/go-toml v1.8.1
+	github.com/itchyny/gojq v0.12.4
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pelletier/go-toml v1.9.3
 	github.com/toqueteos/webbrowser v1.2.0
-	golang.org/x/net v0.0.0-20210326220855-61e056675ecf // indirect
-	golang.org/x/oauth2 v0.0.0-20210323180902-22b0adad7558
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210622215436-a8dc77f794b6
 	google.golang.org/appengine v1.6.7 // indirect
 )
---
+++ pkg/openapi/openapi.go
@@ -0,0 +1,126 @@
+package openapi
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/iancoleman/strcase"
+)
+
+type Operation struct {
+	*flag.FlagSet
+	BaseURL   string
+	Path      string
+	Method    string
+	Variables map[string]map[string]*string
+}
+
+func NewOperation(url, path, method string, op *openapi3.Operation) *Operation {
+	fs := flag.NewFlagSet(op.OperationID, flag.ExitOnError)
+
+	variables := make(map[string]map[string]*string)
+
+	for _, prm := range op.Parameters {
+		argName := strcase.ToKebab(prm.Value.Name)
+
+		if _, ok := variables[prm.Value.In]; !ok {
+			variables[prm.Value.In] = make(map[string]*string)
+		}
+
+		variables[prm.Value.In][prm.Value.Name] = fs.String(argName, "", prm.Value.Description)
+	}
+
+	return &Operation{
+		BaseURL:   url,
+		Path:      path,
+		Method:    method,
+		FlagSet:   fs,
+		Variables: variables,
+	}
+}
+
+func (cmd *Operation) BuildRequest(ctx context.Context, baseURI string) (*http.Request, error) {
+	p := cmd.Path
+
+	for k, v := range cmd.Variables["path"] {
+		if v == nil || len(*v) == 0 {
+			continue
+		}
+
+		p = strings.ReplaceAll(p, "{"+k+"}", *v)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, cmd.Method, baseURI+p, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	if m, ok := cmd.Variables["header"]; !ok {
+		for k, v := range m {
+			if v == nil || len(*v) == 0 {
+				continue
+			}
+
+			req.Header.Add(k, *v)
+		}
+	}
+
+	body := make(url.Values)
+
+	if m, ok := cmd.Variables["formData"]; !ok {
+		for k, v := range m {
+			if v == nil || len(*v) == 0 {
+				continue
+			}
+
+			body.Add(k, *v)
+		}
+	}
+
+	if len(body) > 0 {
+		req.PostForm = body
+	}
+
+	query := make(url.Values)
+
+	if m, ok := cmd.Variables["query"]; !ok {
+		for k, v := range m {
+			if v == nil || len(*v) == 0 {
+				continue
+			}
+
+			query.Add(k, *v)
+		}
+	}
+
+	if len(query) > 0 {
+		req.URL.RawQuery = query.Encode()
+	}
+
+	return req, nil
+}
+
+func GenerateOperation(baseURL string, openapiJSON io.Reader) (map[string]*Operation, error) {
+	cmds := make(map[string]*Operation)
+
+	var doc3 openapi3.T
+	if err := json.NewDecoder(openapiJSON).Decode(&doc3); err != nil {
+		return nil, fmt.Errorf("failed to decode openapi v3: %w", err)
+	}
+
+	for path, v := range doc3.Paths {
+		for method, op := range v.Operations() {
+			cmd := NewOperation(baseURL, path, method, op)
+			cmds[cmd.Name()] = cmd
+		}
+	}
+
+	return cmds, nil
+}
